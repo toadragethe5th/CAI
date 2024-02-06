6369d109ffc805ade20bedd92e72731ba24b1e09
ToadRage <153384304+toadragethe5th@users.noreply.github.com>
2024-02-06 08:15:56 -0500

    e

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdint.h>
+
+#include "c_api_experimental.h"
+#include "c_api_macros.h"
+#include "c_api.h"
+#include "tensor_interface.h"
+#include "tf_attrtype.h"
+#include "tf_datatype.h"
+#include "tf_file_statistics.h"
+#include "tf_status.h"
+#include "tf_tensor.h"
+#include "tf_tstring.h"
